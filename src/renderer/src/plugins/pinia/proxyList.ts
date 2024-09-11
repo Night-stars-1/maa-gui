ca02c7941f43636287ac6b40d42cdfc39a905b1e
Night-stars-1 <nujj1042633805@gmail.com>
2024-09-12 01:31:48 +0800

    fix: 修复检查资源包更新异常

--- src/renderer/src/plugins/pinia/proxyList.ts
+++ src/renderer/src/plugins/pinia/proxyList.ts
@@ -1,11 +1,17 @@
+/*
+ * @Author: Night-stars-1 nujj1042633805@gmail.com
+ * @Date: 2024-09-11 19:15:27
+ * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
+ * @LastEditTime: 2024-09-12 01:31:44
+ */
 import { getProxy, setProxy } from '@renderer/utils/proxyUtils'
 import { defineStore } from 'pinia'
 
 export const useProxyList = defineStore('proxyList', () => {
   const proxyList = [
     {
-      name: 'https://ghp.ci',
-      url: 'https://ghp.ci'
+      name: 'https://ghp.ci/',
+      url: 'https://ghp.ci/'
     },
     {
       name: '不代理',
