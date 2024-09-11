3a9a2be80c789c5cdfaa29095cb08c6d2dffd0eb
Night-stars-1 <nujj1042633805@gmail.com>
2024-09-11 14:36:53 +0800

    chore: 去除调试代码

--- src/renderer/src/plugins/pinia/logMessage.ts
+++ src/renderer/src/plugins/pinia/logMessage.ts
@@ -2,12 +2,12 @@
  * @Author: Night-stars-1 nujj1042633805@gmail.com
  * @Date: 2024-09-07 19:06:43
  * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
- * @LastEditTime: 2024-09-11 14:19:50
+ * @LastEditTime: 2024-09-11 14:36:48
  */
 import { defineStore } from 'pinia'
 
 export const useLogMessage = defineStore('logMessage', () => {
-  const messageList = ref<string[]>(["4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4","4"])
+  const messageList = ref<string[]>([])
 
   window.api.log((_, message) => {
     messageList.value.push(message)
