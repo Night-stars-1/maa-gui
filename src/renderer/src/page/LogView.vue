f214b3c80e631a2a36a4a5d9112c4ddfe43c07b0
Night-stars-1 <nujj1042633805@gmail.com>
2024-09-18 00:58:50 +0800

    feat: 日志页面使用虚拟滚动

--- src/renderer/src/page/LogView.vue
+++ src/renderer/src/page/LogView.vue
@@ -2,17 +2,18 @@
  * @Author: Night-stars-1 nujj1042633805@gmail.com
  * @Date: 2024-09-07 17:06:25
  * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
- * @LastEditTime: 2024-09-18 00:27:48
+ * @LastEditTime: 2024-09-18 00:58:41
 -->
 <script setup lang="ts">
 import { useLogMessage } from '@stores/logMessage'
+import { VVirtualScroll } from 'vuetify/lib/components/index.mjs'
 
-const { messageList } = useLogMessage()
-const scrollContainer = ref<ComponentPublicInstance | null>(null) // 滚动容器的引用
+const { messageList } = storeToRefs(useLogMessage())
+const scrollContainer = ref<VVirtualScroll | null>(null) // 滚动容器的引用
 const isAtBottom = ref(false)
 
 watch(
-  () => messageList,
+  () => messageList.value,
   () => {
     nextTick(() => {
       if (isAtBottom.value) scrollToBottom()
@@ -31,7 +32,10 @@ function onScroll() {
 }
 function scrollToBottom() {
   if (scrollContainer.value) {
-    scrollContainer.value.$el.scrollTop = scrollContainer.value.$el.scrollHeight
+    setTimeout(() => {
+      // scrollContainer.value.$el.scrollTop = scrollContainer.value.$el.scrollHeight
+      scrollContainer.value?.scrollToIndex(messageList.value.length - 1)
+    }, 100)
   }
 }
 onMounted(() => {
@@ -39,7 +43,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <v-list
+  <!-- <v-list
     ref="scrollContainer"
     class="scroll-container"
     :lines="false"
@@ -53,12 +57,20 @@ onMounted(() => {
       :title="message"
       class="log-msg"
     ></v-list-item>
-  </v-list>
+  </v-list> -->
+  <v-virtual-scroll
+    ref="scrollContainer"
+    class="scroll-container"
+    :items="messageList"
+    @scroll="onScroll"
+  >
+    <template #default="{ item }"> {{ item }}</template>
+  </v-virtual-scroll>
 </template>
 
 <style lang="scss" scoped>
 .scroll-container {
-  max-height: 100vh;
+  height: calc(100vh - 20px);
 }
 .log-msg {
   div {
@@ -70,5 +82,13 @@ onMounted(() => {
     margin: 0px !important;
     padding: 0px !important;
   }
+}
+</style>
+
+<style lang="scss">
+.scroll-container .v-virtual-scroll__item {
+  font-size: 13.5px;
+  white-space: pre-wrap;
+  font-family: monospace;
 }
 </style>
